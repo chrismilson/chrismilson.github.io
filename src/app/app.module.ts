c85a962e2787638f93c32037709100f19bd3c5fb
Chris Milson <chris.w.milson@gmail.com>
2020-01-08 16:03:30 +0900

    Add social icons
    Using the angular module for font awesome, I added some simple icons to
    the page. Currently there is one for github, linkedin and an icon to
    download my CV, which doesnt lead anywhere yet.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TyperComponent } from './typer/typer.component';
 import { WallpaperComponent } from './wallpaper/wallpaper.component';
+import { SocialComponent } from './social/social.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TyperComponent,
-    WallpaperComponent
+    WallpaperComponent,
+    SocialComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/social/social.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  faGithub,
+  faLinkedin
+} from '@fortawesome/free-brands-svg-icons';
+import {
+  faIdBadge
+} from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-social',
+  templateUrl: './social.component.html',
+  styles: [`
+    .icon-list {
+      font-size: 1.5rem;
+      position: fixed;
+      top: 0;
+      right: 0;
+
+      display: flex;
+    }
+
+    a {
+      color: inherit;
+      margin: 0.5rem;
+    }
+  `]
+})
+export class SocialComponent {
+  icons = [
+    {
+      name: 'Github',
+      title: 'Check out my Github',
+      icon: faGithub,
+      link: 'https://github.com/chrismilson'
+    },
+    {
+      name: 'Linkedin',
+      title: '...or my Linkedin',
+      icon: faLinkedin,
+      link: 'https://www.linkedin.com/in/chris-milson/'
+    },
+    {
+      name: 'CV',
+      title: 'Have a look at my CV too',
+      icon: faIdBadge,
+      link: 'myCV'
+    }
+  ];
+}
