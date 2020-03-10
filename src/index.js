301852f6449c04691b0f15e2f127131e4e65690b
Chris Milson <chris.w.milson@gmail.com>
2020-03-10 09:31:06 +0900

    Remove serviceworker code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(<App />, document.getElementById('root'))
-
-// The serviceworker is supplying the user-page resources when navigating to a
-// project page. For this reason, we are disabling the service worker.
-serviceWorker.unregister()
