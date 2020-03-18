955c87fda64995520358777d3823b1c1b5b1aaa1
Chris Milson <chris.w.milson@gmail.com>
2020-03-18 11:58:40 +0900

    Add main app file

---
+++ src/ui/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Social from './components/Social'
+import Wallpaper from './components/Wallpaper/Circles'
+
+export default function App() {
+  return (
+    <div className="App">
+      <Social />
+      <h1>Chris Milson</h1>
+      <Wallpaper />
+    </div>
+  )
+}
