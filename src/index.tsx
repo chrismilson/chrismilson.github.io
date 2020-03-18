e669e10973d316f1c522c3d2a68db202cae6b605
Chris Milson <chris.w.milson@gmail.com>
2020-03-18 11:48:43 +0900

    Remove redux
    This site does not have any app state; and thus it does not need redux.

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './ui'
-import { Provider } from 'react-redux'
-import store from './state/store'
+import App from './ui/App'
 import './index.css'
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById('root'))
