d7d27b035c6dc657f26b7a7e22eda3f247b62b9a
Chris Milson <chris.w.milson@gmail.com>
2020-03-18 15:45:17 +0900

    Import the default random wallpaper

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Social from './components/Social'
-import Wallpaper from './components/Wallpaper/Circles'
+import Wallpaper from './components/Wallpaper'
 
 export default function App() {
   return (
