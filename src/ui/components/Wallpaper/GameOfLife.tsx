3f8e1d7f6e422e53a20488998e97d6f80a4e35ea
Chris Milson <chris.w.milson@gmail.com>
2020-03-20 16:07:32 +0900

    Add game of life wallpaper

---
+++ src/ui/components/Wallpaper/GameOfLife.tsx
@@ -0,0 +1,134 @@
+import React from 'react'
+import { CanvasWallpaper } from './common/Wallpaper'
+import { randomInt } from './common/random'
+
+class Cell {
+  private status: number
+  private neighbors: Cell[] = []
+
+  constructor(initiallyAlive: boolean) {
+    this.status = initiallyAlive ? 1 : 0
+  }
+
+  get isAlive() {
+    return this.status & 1
+  }
+
+  swap() {
+    this.status ^= 1
+  }
+
+  addNeighbor(neighbor: Cell) {
+    this.neighbors.push(neighbor)
+  }
+
+  check() {
+    const liveNeighbors = this.neighbors.reduce((s, n) => s + n.isAlive, 0)
+
+    if ((liveNeighbors | this.status) === 3) this.status |= 2
+  }
+
+  age() {
+    this.status = this.status >> 1
+  }
+}
+
+class Game {
+  private width: number
+  private height: number
+  /**
+   * A cell is a number representing different states.
+   * The bits from least significan to most significant mean:
+   * - The cells current status.
+   * - The cells next status.
+   */
+  private board: Cell[][]
+
+  constructor(width: number, height: number) {
+    this.width = Math.floor(width)
+    this.height = Math.floor(height)
+    this.board = []
+
+    const probabilityOfLife = 0.1
+    for (let i = 0; i < this.width; i++) {
+      this.board[i] = []
+      for (let j = 0; j < this.height; j++) {
+        this.board[i][j] = new Cell(Math.random() < probabilityOfLife)
+      }
+    }
+
+    // addd the cell's neighbors
+    for (let i = 0; i < this.width; i++) {
+      for (let j = 0; j < this.height; j++) {
+        for (const [dx, dy] of [
+          [-1, -1],
+          [-1, 0],
+          [-1, 1],
+          [0, -1],
+          [0, 1],
+          [1, -1],
+          [1, 0],
+          [1, 1]
+        ]) {
+          const x = (i + dx + this.width) % this.width
+          const y = (j + dy + this.height) % this.height
+          this.board[i][j].addNeighbor(this.board[x][y])
+        }
+      }
+    }
+  }
+
+  draw(ctx: CanvasRenderingContext2D) {
+    for (let i = 0; i < this.width; i++) {
+      for (let j = 0; j < this.height; j++) {
+        if (this.board[i][j].isAlive) {
+          ctx.save()
+          ctx.fillStyle = `hsl(${randomInt(360)},100%,40%)`
+          ctx.fillRect(i, j, 1, 1)
+          ctx.restore()
+        }
+      }
+    }
+  }
+
+  move() {
+    this.board.forEach(row => row.forEach(cell => cell.check()))
+    this.board.forEach(row => row.forEach(cell => cell.age()))
+
+    const randomSwaps = 5
+    for (let i = 0; i < randomSwaps; i++) {
+      this.board[randomInt(this.width)][randomInt(this.height)].swap()
+    }
+  }
+}
+
+const GameOfLife: React.FC = () => (
+  <CanvasWallpaper
+    className="GameOfLife"
+    draw={(ctx, widthPX, heightPX) => {
+      // transform the canvas
+      const width = widthPX / 10
+      const height = heightPX / 10
+      ctx.setTransform(10, 0, 0, 10, 0, 0)
+      ctx.globalAlpha = 0.2
+
+      const game = new Game(width, height)
+
+      let interval: number
+
+      const draw = () => {
+        ctx.clearRect(0, 0, width, height)
+
+        game.draw(ctx)
+        game.move()
+      }
+
+      setInterval(draw, 100)
+      return () => {
+        clearInterval(interval)
+      }
+    }}
+  />
+)
+
+export default GameOfLife
--- src/ui/components/Wallpaper/index.ts
+++ src/ui/components/Wallpaper/index.ts
@@ -1,7 +1,8 @@
 import Circles from './Circles'
 import Perspectives from './Perspectives'
 import BouncyBalls from './BouncyBalls'
+import GameOfLife from './GameOfLife'
 
-const allWallpapers = [Circles, Perspectives, BouncyBalls]
+const allWallpapers = [Circles, Perspectives, BouncyBalls, GameOfLife]
 
 export default allWallpapers[Math.floor(Math.random() * allWallpapers.length)]
