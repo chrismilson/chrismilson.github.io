75aea47e8d46e4386ee526bf81683280410e79c4
Chris Milson <chris.w.milson@gmail.com>
2020-04-09 13:39:21 +0900

    Add normally distributed random

--- src/ui/components/Wallpaper/common/random.ts
+++ src/ui/components/Wallpaper/common/random.ts
@@ -16,3 +16,13 @@ export const randomInt = (from: number, to: number = from) => {
 export const randomRange = (from: number, to: number) => {
   return from + Math.random() * (to - from)
 }
+
+/** Returns a random normally distributed float */
+export const randomNormal = (mean = 0, stddev = 1) => {
+  let u = 0
+  let v = 0
+  while (u === 0) u = Math.random()
+  while (v === 0) v = Math.random()
+  const statistic = Math.sqrt(-2 * Math.log(u)) + Math.cos(2 * Math.PI * v)
+  return statistic * stddev + mean
+}
