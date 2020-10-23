c7427583ae2805f9539695a8a2898c9bc2fdd699
Chris Milson <chris.w.milson@gmail.com>
2020-10-23 14:04:08 +0900

    Update import
    The import now reflects the name of the module.

--- src/ui/components/Wallpaper/StarMap/index.tsx
+++ src/ui/components/Wallpaper/StarMap/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { OffscreenCanvasWallpaper } from '../common/Wallpaper'
 
-const init = (m => () => m)(import('worker-loader!./draw'))
+const init = (m => () => m)(import('worker-loader!./draw.worker.ts'))
 
 export const StarMap: React.FC = () => {
   return (
